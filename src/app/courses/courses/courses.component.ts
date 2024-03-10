a15fff6bd66ff528cfe6eef3a45052ea3ec0ca53
ana-msilva <ana.msilva@ibm.com>
2024-03-10 18:24:13 -0300

    feat: Lista de Cursos: Tratamento de Erros e MatDialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable, delay, first, tap } from 'rxjs';
+import { Observable, catchError, delay, first, of, tap } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -12,12 +14,22 @@ import { Observable, delay, first, tap } from 'rxjs';
 export class CoursesComponent implements OnInit {
 
   private _courseService = inject(CoursesService);
+  public dialog = inject(MatDialog);
 
   public courses$!: Observable<Course[]>;
   public displayedColumns = ['name', 'category'];
 
   public ngOnInit(): void {
-    this.courses$ = this._courseService.list().pipe(first(), delay(2000), tap(courses => console.log(courses)));
+    this.courses$ = this._courseService.list().pipe(first(),
+      catchError(error => { this.onError('Erro ao carregar cursos'); return of([]) }
+      ),
+      delay(1000), tap(courses => console.log(courses)));
+  }
+
+  public onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
 }
