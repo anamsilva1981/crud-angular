11866a827022a26f3f2de6c514782231852878b3
ana-msilva <ana.msilva@ibm.com>
2024-03-10 17:14:22 -0300

    Lista de Cursos: Spinner (Carregando)

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable, tap } from 'rxjs';
+import { Observable, delay, first, tap } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -13,11 +13,11 @@ export class CoursesComponent implements OnInit {
 
   private _courseService = inject(CoursesService);
 
-  public courses!: Observable<Course[]>;
+  public courses$!: Observable<Course[]>;
   public displayedColumns = ['name', 'category'];
 
   public ngOnInit(): void {
-    this.courses = this._courseService.list().pipe(tap(courses => console.log(courses)));
+    this.courses$ = this._courseService.list().pipe(first(), delay(2000), tap(courses => console.log(courses)));
   }
 
 }
