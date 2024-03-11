ace5e87885b12323d4cc86cee9ba5d315a8b72b6
ana-msilva <ana.msilva@ibm.com>
2024-03-11 16:01:20 -0300

    feat: Formulário para Criar Cursos

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  private _fb = inject(FormBuilder);
 
-  ngOnInit(): void {
+
+  public form!: FormGroup;
+
+
+  public ngOnInit(): void {
+
+    this.form = this._fb.group({
+      name: [null],
+      category: [null],
+    })
   }
 
 }
