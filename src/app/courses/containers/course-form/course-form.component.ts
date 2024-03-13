f623430730d5ce7e0c053a7d3bb47a671b97640b
ana-msilva <ana.msilva@ibm.com>
2024-03-13 11:33:04 -0300

    feat: Lógica para popular o Formulário

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../../services/courses.service';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-course-form',
@@ -15,15 +17,26 @@ export class CourseFormComponent implements OnInit {
   private _courseService = inject(CoursesService);
   private _snackBar = inject(MatSnackBar);
   private _location = inject(Location)
+  private _route = inject(ActivatedRoute);
 
   public form!: FormGroup;
+
 
   public ngOnInit(): void {
 
     this.form = this._fb.group({
+      _id: [''],
       name: [''],
       category: [''],
     })
+
+    const course: Course = this._route.snapshot.data['course'];
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    })
+    
   }
 
   public onSubmit() {
